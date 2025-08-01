e84e1329a1a71950b9d61b583d35a1a942508473
JeongMinJe <tobesincere@naver.com>
2025-08-01 15:54:10 +0900

    add: 일기저장 기능

--- client/src/components/diary-editer/DiaryContent.tsx
+++ client/src/components/diary-editer/DiaryContent.tsx
@@ -1,13 +1,45 @@
+import axios from "axios";
+import { useRef } from "react";
+
 // 부모로부터 onOpenChat 함수를 prop으로 받기 위한 타입 정의
 interface DiaryContentProps {
   onOpenChat: () => void;
 }
 
 const DiaryContent = ({ onOpenChat }: DiaryContentProps) => {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  /**
+   * 클릭했을 때 그 내용을 백엔드로 전달해야 함.
+   * 일단 오늘은 그것부터!
+   */
+
+  const saveDiary = async () => {
+    try {
+      const content = textAreaRef.current?.value;
+
+      // content 가 없을 경우도 처리해야 함.
+
+      const newDiary = {
+        user_id: "CWD91jDBLyyNNo4jbNKK",
+        emotion_score: 5,
+        title: "테스트 일기",
+        content,
+      };
+
+      const response = await axios.post(
+        "http://localhost:4000/api/diaries",
+        newDiary
+      );
+
+      console.log(response);
+    } catch (error) {}
+  };
+
   return (
     <div className="bg-white rounded-xl shadow-md p-6 flex flex-col h-full">
       {/* 일기 입력 영역 */}
       <textarea
+        ref={textAreaRef}
         className="flex-grow w-full p-3 border border-slate-200 rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 hover:border-slate-400 transition-colors"
         placeholder="오늘 하루는 어땠나요?"
       />
@@ -22,7 +54,10 @@ const DiaryContent = ({ onOpenChat }: DiaryContentProps) => {
         </button>
 
         {/* 일기 저장 버튼 (주요 버튼) */}
-        <button className="w-full py-2.5 px-4 font-medium bg-slate-500 text-white border border-transparent rounded-full transition-colors hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-400">
+        <button
+          onClick={saveDiary}
+          className="w-full py-2.5 px-4 font-medium bg-slate-500 text-white border border-transparent rounded-full transition-colors hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-400"
+        >
           일기 저장
         </button>
       </div>
